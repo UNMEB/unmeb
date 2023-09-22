809bd18e9f9affe293956eb8240636b1c663c446
Paul Obunga <paulobunga.one@gmail.com>
2023-09-22 10:50:17 +0300

    Updated Code

---
+++ resources/views/brands/footer.blade.php
@@ -0,0 +1,10 @@
+<div class="d-flex justify-content-center">
+
+    <p class="my-4 small m-n">
+        © Copyright {{date('Y')}}
+        <a href="{{ config('app.url') }}" target="_blank">
+            {{ config('app.name') }}
+        </a>
+    </p>
+
+</div>
