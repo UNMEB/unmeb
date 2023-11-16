f392753bf6068ecadb23b60d809e7d07b5b44a13
Paul Obunga <paulobunga.one@gmail.com>
2023-11-16 11:46:11 +0300

    Updated NSIN Registration Periods UI

--- app/Orchid/Screens/Registration/NSIN/NsinRegistrationPeriodListScreen.php
+++ app/Orchid/Screens/Registration/NSIN/NsinRegistrationPeriodListScreen.php
@@ -3,6 +3,16 @@
 namespace App\Orchid\Screens\Registration\NSIN;
 
 use App\Models\NsinRegistrationPeriod;
+use App\Models\Year;
+use Illuminate\Database\Eloquent\Relations\Relation;
+use Orchid\Screen\Actions\Button;
+use Orchid\Screen\Actions\ModalToggle;
+use Orchid\Screen\Components\Cells\DateTimeSplit;
+use Orchid\Screen\Fields\DateTimer;
+use Orchid\Screen\Fields\Group;
+use Orchid\Screen\Fields\Input;
+use Orchid\Screen\Fields\Relation as FieldsRelation;
+use Orchid\Screen\Fields\Select;
 use Orchid\Screen\Screen;
 use Orchid\Screen\TD;
 use Orchid\Support\Facades\Layout;
@@ -39,7 +49,19 @@ class NsinRegistrationPeriodListScreen extends Screen
      */
     public function commandBar(): iterable
     {
-        return [];
+        return [
+            ModalToggle::make('Add Period')
+            ->modal('createPeriodModal')
+            ->method('create')
+            ->icon('plus'),
+            ModalToggle::make('Import Periods')
+            ->modal('uploadPeriodsModal')
+            ->method('upload')
+            ->icon('upload'),
+            Button::make('Export Data')
+            ->method('download')
+            ->rawClick(false)
+        ];
     }
 
     /**
@@ -56,8 +78,131 @@ class NsinRegistrationPeriodListScreen extends Screen
                 TD::make('month', 'Month'),
                 TD::make('year.year', 'Year'),
                 TD::make('flag', 'Is Active'),
-                TD::make('flag', 'Is Active')->render(fn ($data) => $data->flag == 1 ? 'Active' : 'Inactive')
-            ])
+                TD::make('flag', 'Is Active')->render(fn ($data) => $data->flag == 1 ? 'Active' : 'Inactive'),
+                TD::make('created_at', __('Created On'))
+                ->usingComponent(DateTimeSplit::class)
+                    ->align(TD::ALIGN_RIGHT)
+                    ->sort(),
+
+                TD::make('updated_at', __('Last Updated'))
+                ->usingComponent(DateTimeSplit::class)
+                    ->align(TD::ALIGN_RIGHT)
+                    ->sort(),
+                TD::make(__('Actions'))
+                    ->width(200)
+                    ->cantHide()
+                    ->align(TD::ALIGN_CENTER)
+                    ->render(function (NsinRegistrationPeriod $period) {
+                        $editButton = ModalToggle::make('Edit Period')
+                        ->modal('editPeriodModal')
+                        ->modalTitle('Edit Period ' . $period->year)
+                            ->method('edit') // You can define your edit method here
+                            ->asyncParameters([
+                                'period' => $period->id,
+                            ])
+                            ->class('btn btn-sm btn-success')
+                            ->render();
+
+                        $deleteButton = Button::make('Delete')
+                            ->confirm('Are you sure you want to delete this period?')
+                            ->method('delete', [
+                                'id' => $period->id
+                            ])
+                            ->class('btn btn-sm btn-danger')
+                            ->render();
+
+                        return Group::make([
+                            $editButton,
+                            $deleteButton
+                        ])->fullWidth();
+                    })
+
+            ]),
+
+            Layout::modal('createPeriodModal', Layout::rows([
+
+                FieldsRelation::make('period.year_id')
+                ->title('Period Year')
+                ->fromModel(Year::class, 'year', 'id')
+                    ->horizontal(),
+
+                Select::make('period.month')
+                ->options([
+                    'January'  => 'January',
+                    'February'  => 'February',
+                    'March'  => 'March',
+                    'April'  => 'April',
+                    'May'  => 'May',
+                    'June'  => 'June',
+                    'July'  => 'July',
+                    'August'  => 'August',
+                    'September'  => 'September',
+                    'October'  => 'October',
+                    'November'  => 'November',
+                    'December'  => 'December',
+                ])
+                    ->title('Period Month')
+                    ->horizontal(),
+
+
+                Select::make('period.flag')
+                ->options([
+                    1  => 'Active',
+                    0  => 'Inactive',
+                ])
+                    ->title('Flag')
+                    ->help('Status for Active/Inactive period flag')
+                    ->horizontal()
+                    ->empty('No select')
+            ]))
+                ->title('Create Period')
+                ->applyButton('Create Period'),
+
+            Layout::modal('editPeriodModal', Layout::rows([
+
+                FieldsRelation::make('period.year_id')
+                ->title('Period Year')
+                ->fromModel(Year::class, 'year', 'id')
+                    ->horizontal(),
+
+                Select::make('period.month')
+                ->title('Period Month')
+                ->options([
+                    'January'  => 'January',
+                    'February'  => 'February',
+                    'March'  => 'March',
+                    'April'  => 'April',
+                    'May'  => 'May',
+                    'June'  => 'June',
+                    'July'  => 'July',
+                    'August'  => 'August',
+                    'September'  => 'September',
+                    'October'  => 'October',
+                    'November'  => 'November',
+                    'December'  => 'December',
+                ])
+                    ->horizontal(),
+
+                Select::make('period.flag')
+                ->options([
+                    1  => 'Active',
+                    0  => 'Inactive',
+                ])
+                    ->title('Flag')
+                    ->help('Status for Active/Inactive period flag')
+                    ->horizontal()
+                    ->empty('No select')
+            ]))->async('asyncGetPeriod'),
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function asyncGetPeriod(NsinRegistrationPeriod $period): iterable
+    {
+        return [
+            'period' => $period,
         ];
     }
 }
