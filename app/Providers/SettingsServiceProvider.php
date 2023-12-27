32d4de660bfcda17b18c425ed1462aec48b72072
Paul Obunga <paulobunga.one@gmail.com>
2023-12-27 16:43:11 +0300

    Updated Code

--- app/Providers/SettingsServiceProvider.php
+++ app/Providers/SettingsServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Models\Settings;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\QueryException;
 
 class SettingsServiceProvider extends ServiceProvider
 {
@@ -21,7 +22,26 @@ class SettingsServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $settings = Settings::pluck('value', 'key')->toArray();
-        Config::set('settings', $settings);
+        try {
+            // Check if the Settings model exists and the table is migrated
+            if ($this->app->runningInConsole() || $this->settingsTableExists()) {
+                $settings = Settings::pluck('value', 'key')->toArray();
+                Config::set('settings', $settings);
+            }
+        } catch (QueryException $e) {
+            // Log or handle the error if needed
+            // For example, you might log the exception message or handle it gracefully
+            // Log::error($e->getMessage());
+        }
+    }
+
+    /**
+     * Check if the settings table exists.
+     *
+     * @return bool
+     */
+    private function settingsTableExists(): bool
+    {
+        return \Schema::hasTable((new Settings())->getTable());
     }
 }
