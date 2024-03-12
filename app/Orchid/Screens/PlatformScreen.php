39abba2c8e3ad781e9113d24f1c80536e1755f1e
Paul Obunga <paulobunga.one@gmail.com>
2024-03-12 19:03:03 +0300

    Updated Code

--- app/Orchid/Screens/PlatformScreen.php
+++ app/Orchid/Screens/PlatformScreen.php
@@ -91,7 +91,7 @@ class PlatformScreen extends Screen
                 'biometric_enrollment' => BiometricEnrollment::count(),
                 'pending_nsin' => number_format($pendingNsin),
                 'verified_nsin' => number_format($verifiedNsin),
-                'account_balance' => number_format((float) Account::sum('balance'), 0)
+                'account_balance (UGX)' => number_format((float) Account::sum('balance'), 0)
             ],
             'student_registration_by_course' => $studentRegistrationByCourse,
             'student_registration_by_institution' => $studentRegistrationByInstitution,
