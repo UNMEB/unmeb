095741481d44c33ad3f567d279e4dfc5b7b30f03
Paul Obunga <paulobunga.one@gmail.com>
2024-04-02 06:06:54 +0300

    Updated Code

---
+++ database/migrations/2024_04_02_030433_add_unique_refugee_number_to_students_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('students', function (Blueprint $table) {
+            $table->string('refugee_number')->unique()->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('students', function (Blueprint $table) {
+            $table->dropUnique(['refugee_number']);
+            $table->dropColumn('refugee_number');
+        });
+    }
+};
