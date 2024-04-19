acd5ba180893415c28a9623a434ecef39f6ce47c
Paul Obunga <paulobunga.one@gmail.com>
2024-04-19 14:32:21 +0300

    Updated Code

--- app/Orchid/Screens/NsinApplicationListScreen.php
+++ app/Orchid/Screens/NsinApplicationListScreen.php
@@ -48,7 +48,8 @@ class NsinApplicationListScreen extends Screen
                 ])
             ->from('students as s')
             ->join('nsin_student_registrations as nsr', 's.id', '=', 'nsr.student_id')
-            ->join('nsin_registrations as nr', 'nsr.nsin_registration_id', '=', 'nr.id');
+            ->join('nsin_registrations as nr', 'nsr.nsin_registration_id', '=', 'nr.id')
+            ->whereNotNull('s.institution_id');
 
         if(auth()->user()->inRole('institution')) {
             $baseQuery->where('nr.institution_id', auth()->user()->institution_id);
