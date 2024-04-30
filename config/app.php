71f542a8db1343b3e91a863e4a2fdb59db68a8c0
Paul Obunga <paulobunga.one@gmail.com>
2024-04-30 18:58:47 +0300

    Updated Code

--- config/app.php
+++ config/app.php
@@ -171,7 +171,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\TelescopeServiceProvider::class,
         SettingsServiceProvider::class,
 
         RealRashid\SweetAlert\SweetAlertServiceProvider::class,
