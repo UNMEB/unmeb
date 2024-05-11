5fe85af9b47d512f7fae25ccdd57b94b2fb686f9
Paul Obunga <paulobunga.one@gmail.com>
2024-05-11 08:34:31 +0300

    Updated Code: Changed maintenance page

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -2,4 +2,4 @@
 
 @section('title', __('Under Maintenance'))
 @section('code', '503')
-@section('message', __('UNMEB system is undergoing upgrade. Please check again later.'))
+@section('message', __('System upgrade in progress. Please check again later.'))
