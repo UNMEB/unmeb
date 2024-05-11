70b83f43c661060031c5b28b9659274b96af368e
Paul Obunga <paulobunga.one@gmail.com>
2024-05-11 08:35:07 +0300

    Updated Code: Changed maintenance page

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -2,4 +2,4 @@
 
 @section('title', __('Under Maintenance'))
 @section('code', '503')
-@section('message', __('System upgrade in progress. Please check again later.'))
+@section('message', __('System upgrade in progress.'))
