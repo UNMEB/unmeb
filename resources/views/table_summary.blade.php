8738850873cb706889bf646af1e80ba1ae29ff95
Paul Obunga <paulobunga.one@gmail.com>
2024-05-13 15:58:06 +0300

    Updated Code

--- resources/views/table_summary.blade.php
+++ resources/views/table_summary.blade.php
@@ -4,20 +4,20 @@
         <div class="card-body">
             <div class="row">
                 <div class="col-md-3">
-                    <h5 class="card-title">Total Students Registered</h5>
-                    <p class="card-text">1000</p>
+                    <h5 class="card-title">Total Students</h5>
+                    <p class="card-text">0</p>
                 </div>
                 <div class="col-md-3 d-none d-md-block border-start">
                     <h5 class="card-title">Total Approved</h5>
-                    <p class="card-text">800</p>
+                    <p class="card-text">0</p>
                 </div>
                 <div class="col-md-3 d-none d-md-block border-start">
                     <h5 class="card-title">Total Rejected</h5>
-                    <p class="card-text">100</p>
+                    <p class="card-text">0</p>
                 </div>
                 <div class="col-md-3 d-none d-md-block border-start">
                     <h5 class="card-title">Total Pending</h5>
-                    <p class="card-text">100</p>
+                    <p class="card-text">0</p>
                 </div>
             </div>
         </div>
